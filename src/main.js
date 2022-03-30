e53485157be2165020d1d68b7b20fd38e19bd088
xs-web-lyq <3235951041@qq.com>
2022-03-30 10:11:14 +0800

    vue3_test-first-commit

---
+++ src/main.js
@@ -0,0 +1,16 @@
+// 引入的不再是Vue的构造函数，引入的是一个名为createApp的工厂函数
+import { createApp } from 'vue'
+import App from './App.vue'
+const app = createApp(App);
+// console.log(app);
+app.mount('#app')
+
+setTimeout(()=>{
+    app.unmount('#app')
+},1000)
+
+// vue2中挂载的步骤,在Vue3项目中不能使用Vue2的挂载方式。不兼容
+// const vm = new Vue({
+//     render: h => h(App)
+// })
+// vm.$mount('#app')
